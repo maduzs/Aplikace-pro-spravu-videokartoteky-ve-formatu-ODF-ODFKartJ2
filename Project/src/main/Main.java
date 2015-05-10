e9dcd0d75cfe958c81921200d9695d41203a1d45
suckitnewbie <suckitnewbie@gmail.com>
2015-05-10 17:50:01 +0200

    Project and Basic GUI created

---
+++ Project/src/main/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import gui.MainWindow;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author michaltaraj
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame mainWindow = new MainWindow();
+
+                //Display the window.
+                mainWindow.pack();
+                mainWindow.setVisible(true);
+    }
+    
+}
